b304ae3882115d9ed665ca8a419741f4e64c0943
Justin Kim <hjk.businessemail@gmail.com>
2022-02-13 23:17:24 -0800

    tests

---
+++ src/main/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    win.loadFile("src/renderer/index.html");
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
