da2deb059bd1f8db6697d9798479254e438602f5
Justin Kim <hjk.businessemail@gmail.com>
2022-04-03 14:57:39 -0700

    Update Environment.py

--- AI/Environment.py
+++ AI/Environment.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sap
 
 
 class Environment:
@@ -6,72 +7,19 @@ class Environment:
     def __init__(self, avg_window, transport_time=50):
         self.list = []
 
-        # Brands and their properties
-        self.n_brands = 4
-        self.brand_appearance_probability = np.array([0.25, 0.25, 0.25, 0.25])
-        self.brand_sale_price = np.array([18, 28, 31, 59])
-
-        self.average_brand_related_transport_cost = np.array([0.5, 2.5, 4.5, 18])
-        self.brand_related_transport_cost_variance = np.array([1.5, 1.5, 1.5, 1.5])
-
-        self.average_brand_repair_price = np.array([1, 4, 5, 24.5])
-        self.brand_repair_variance = np.array([2, 2, 2, 1])
-
-        # Non brand-related costs detectable by patterns in the state
-        self.n_transport_conditions = 2
-        self.transport_time = transport_time
-        self.transport_cost = np.array([0.1, 7])
-
-        self.transport_condition_probability = np.array([0.1, 0.05])
-
-        # Instance variables which will be part of the state
-        self.brand = -1
-        self.repaired = None
-        self.time = None
-        self.transport_condition = None
-
-        # For tracking performance
-        # Hardcoded in this setup, only used for performance evaluation
-        self.optimal_actions = np.array([1, 0, 0, 1])
-        self.optimal_choices = 0
-        self.optimal_actions_list = []
-        self.avg_window = avg_window
+        # sap
+        self.sim = sap.SAP()
 
     def reset(self):
-        # Randomly choose a brand which the agent faces in this episode.
-        self.brand = np.random.choice(self.n_brands, size=1, p=self.brand_appearance_probability)[0]
-        self.repaired = 0
-        self.time = 0
-        self.transport_condition = np.zeros(shape=(self.n_transport_conditions,))
-        return np.array([self.repaired] + self.transport_condition.tolist() + [self.brand, self.time], dtype=np.int32)
+        self.sim = sap.SAP()
+        return np.array(self.sim.get_scaled_state, dtype=np.int32)
 
     def step(self, action):
-        reward = 0
-        if self.time == 0:
-            # Check if the repair action was executed.
-            self.repaired = action == 0
-            if self.repaired:
-                # If repair was chosen, immediately charge the repair cost
-                repair_price = np.random.normal(self.average_brand_repair_price[self.brand],
-                                                self.brand_repair_variance[self.brand])
-                reward -= repair_price
+        reward = -1000
+        
+        result = self.sim.step()
 
-            # Performance tracking: tracks the number of good decisions for the whole training.
-            if action == self.optimal_actions[self.brand]:
-                self.optimal_choices += 1
-            # Update moving-mean window of latest optimal actions
-            self.optimal_actions_list.append(action == self.optimal_actions[self.brand])
-            if len(self.optimal_actions_list) > self.avg_window:
-                del self.optimal_actions_list[0]
-
-        self.time += 1
-        done = self.time == self.transport_time
-
-        # Generate transport events which increase delivery costs.
-        transport_cond = self.transport_condition
-        transport_cond += np.array(
-            [np.random.random() < self.transport_condition_probability[i] for i in range(self.n_transport_conditions)])
-        self.transport_condition = transport_cond
+        reward += self.sim.score
 
         if done and self.repaired:
             # Calculate the final reward by adding costs and sales price.
