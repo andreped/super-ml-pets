3ae91e2ffe15d89afffa43422ba2db12fa31ca80
andreped <andrped94@gmail.com>
2022-08-05 23:07:38 +0200

    refactored. V6.0 seems to be working

--- V5.0/AI/test.py
+++
@@ -1,7 +0,0 @@
-import glob, os
-
-
-list_of_files = glob.glob('ckpt/*')
-latest_file = max(list_of_files, key=os.path.getctime)
-
-print(latest_file.name)
--- v2.0/process-results.py
+++
@@ -1,2 +0,0 @@
-p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-%i' % CHECKPOINT)
-winner = p.run(eval_genomes, 1)
