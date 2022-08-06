49861d4df40a38cd6f038e7704162988b820ff3d
andreped <andrped94@gmail.com>
2022-08-06 23:44:51 +0200

    forgot imports

--- tests/test_train.py
+++ tests/test_train.py
@@ -13,7 +13,6 @@ import numpy as np
 from tensorflow import keras
 from tqdm import tqdm
 
-from utils import Data, get_qs, save_logs
 from models import agent
 
 import random
@@ -26,6 +25,48 @@ from sapai import Player
 from sapai import Food
 from sapai import Team
 from sapai.battle import Battle
+
+class Data():
+    # Save the teams from every level, refresh every generation to fight against
+    past_teams = [[]]
+
+    total_wins = 0
+    total_losses = 0
+    total_draws = 0
+
+    logs = []
+
+def save_logs(data):
+    with open('past_teams', 'w', newline='') as f:
+        a = csv.writer(f)
+        a.writerows(data.past_teams)
+
+    with open('past_teams_bin', 'wb') as f:
+        pickle.dump(data.past_teams, f)
+
+    with open('logs', 'w', newline='') as f:
+        a = csv.writer(f)
+        for l in data.logs:
+            a.writerow([str(l)])
+    data.logs = []
+
+def agent(state_shape, action_shape):
+    """ The agent maps X-states to Y-actions
+    e.g. The neural network output is [.1, .7, .1, .3]
+    The highest value 0.7 is the Q-Value.
+    The index of the highest action (0.7) is action #1.
+    """
+    learning_rate = 0.001
+    init = tf.keras.initializers.HeUniform()
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(24, input_shape=state_shape, activation='relu', kernel_initializer=init))
+    model.add(keras.layers.Dense(12, activation='relu', kernel_initializer=init))
+    model.add(keras.layers.Dense(action_shape, activation='linear', kernel_initializer=init))
+    model.compile(loss=tf.keras.losses.Huber(), optimizer=tf.keras.optimizers.Adam(lr=learning_rate))  # , metrics=['accuracy'])
+    return model
+
+def get_qs(model, state, step):
+    return model.predict(state.reshape([1, state.shape[0]]), verbose=0)[0]
 
 
 class SAP(object):
