a920293714b46ea2dacccda1885144b1d548c458
andreped <andrped94@gmail.com>
2022-08-06 18:57:52 +0200

    started making eval script

--- V6.0/eval.py
+++ V6.0/eval.py
@@ -3,8 +3,9 @@ Script for evaluating trained RL model. Run battles of new models against
 older models, record battle results, and calculate metrics.
 """
 
-def apply(best_model, target_models):
-
+def apply(best_model: str, target_models: str):
+	print(":)")
+	pass
 
 if __name__ == "__main__":
-	main()
+	apply("a", "b")
