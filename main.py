1b4ffe39bbcd98423a8de27f901f966c0e62a4f4
andreped <andrped94@gmail.com>
2022-08-21 19:15:49 +0200

    more refactoring [no ci]

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     parser.add_argument('--model_name', metavar='--m', type=str, nargs='?', default="rl_model",
                         help="which name to use for the model.")
     parser.add_argument('--nb_retries', metavar='--nr', type=int, nargs='?', default=1,
-                        help="number of times training is restarted (continued) if it crashes. Set to -1 to train forever.")
+                        help="number of times training is restarted (continued) if it crashes. Set to -1 to train "
+                             "forever.")
     parser.add_argument('--infer_model', metavar='-im', type=str, nargs='?', default=None,
                         help="which model to use for deployment (give full path, without extension '.zip'.")
     parser.add_argument('--infer_pversion', metavar='-py', type=str, nargs='?', default="3.7",
