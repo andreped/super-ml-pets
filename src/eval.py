0932f7c716c1871b3785818bb6fd1b9e16fe325b
andreped <andrped94@gmail.com>
2022-09-04 17:26:37 +0200

    worked on Evalute class

--- src/eval.py
+++ src/eval.py
@@ -3,12 +3,16 @@ Script for evaluating trained RL model. Run battles of new models against
 older models, record battle results, and calculate metrics.
 """
 
-from tensorflow.keras.models import load_model
-from utils import Data
-import sap
+from utils import opponent_generator
+import sapai
 from tqdm import tqdm
 import numpy as np
 from collections import deque
+from sb3_contrib import MaskablePPO
+from sb3_contrib.common.maskable.evaluation import evaluate_policy
+from sb3_contrib.common.maskable.utils import get_action_masks
+from sapai_gym import SuperAutoPetsEnv
+from sapai_gym.opponent_gen.opponent_generators import random_opp_generator, biggest_numbers_horizontal_opp_generator
 
 
 class Evaluate:
@@ -22,16 +26,53 @@ class Evaluate:
         self.test_episodes = test_episodes
         self.best_model = None
         self.target_model = None
+        self.env = SuperAutoPetsEnv(opponent_generator, valid_actions_only=True)
 
-    def load_models(self):
-        self.best_model = load_model(self.best_model_path, compile=False)
-        self.target_model = load_model(self.target_model_path, compile=False)
+    def load_models_and_set_env(self):
+        self.best_model = MaskablePPO.load(self.best_model_path, compile=False)
+        self.target_model = MaskablePPO.load(self.target_model_path, compile=False)
 
-    def setup(self):
-        pass
+        self.best_model.set_env(self.env)
+        self.target_model.set_env(self.env)
 
     def battle(self):
-        pass
+        for episode in tqdm(range(self.test_episodes), "Iter:"):
+            total_training_rewards = 0
+            observation = self.env.get_scaled_state()
+            done = False
+
+            while not done:
+                # Exploit best known action
+                # model dims are (batch, env.observation_space.n)
+                encoded = observation
+                encoded_reshaped = encoded.reshape([1, encoded.shape[0]])
+                action = best_model.predict(encoded_reshaped, verbose=0).flatten()
+
+                self.env.step(action)
+
+                action = np.argmax(action)
+                new_observation = self.env.get_scaled_state()
+                reward = self.env.score
+                done = self.env.isGameOver()
+                info = None
+                replay_memory.append([observation, action, reward, new_observation, done])
+
+                observation = new_observation
+                total_training_rewards += reward
+
+                if done:
+                    if episode % verbose_step == 0:
+                        print(
+                            'After n steps = {} : Total training rewards: {} '.format(episode, total_training_rewards))
+                    total_training_rewards += 1
+                    break
+
+            data.total_wins += self.env.wins
+            data.total_losses += self.env.losses
+            data.total_draws += self.env.draws
+
+            if episode % verbose_step == 0:
+                print("stats: ", data.total_wins, "/", data.total_draws, "/", data.total_losses)
 
 
 def apply(best_model_path: str, target_model_path: str, test_episodes: int):
@@ -39,57 +80,12 @@ def apply(best_model_path: str, target_model_path: str, test_episodes: int):
     method for evaluating agent in simulated Super Auto Pets environment
     """
     # load models
-    best_model = load_model(best_model_path, compile=False)
-    target_model = load_model(target_model_path, compile=False)
-
-    # initialize environment
-    data = Data()
-    env = sap.SAP(data)
-    print(env)
-    exit()
-    replay_memory = deque(maxlen=50_000)
-    verbose_step = 1
-
-    for episode in tqdm(range(test_episodes), "Iter:"):
-        total_training_rewards = 0
-        env = sap.SAP(data)
-        observation = env.get_scaled_state()
-        done = False
-
-        while not done:
-            # Exploit best known action
-            # model dims are (batch, env.observation_space.n)
-            encoded = observation
-            encoded_reshaped = encoded.reshape([1, encoded.shape[0]])
-            action = best_model.predict(encoded_reshaped, verbose=0).flatten()
-
-            env.step(action)
-
-            action = np.argmax(action)
-            new_observation = env.get_scaled_state()
-            reward = env.score
-            done = env.isGameOver()
-            info = None
-            replay_memory.append([observation, action, reward, new_observation, done])
-
-            observation = new_observation
-            total_training_rewards += reward
-
-            if done:
-                if episode % verbose_step == 0:
-                    print('After n steps = {} : Total training rewards: {} '.format(episode, total_training_rewards))
-                total_training_rewards += 1
-                break
-
-        data.total_wins += env.wins
-        data.total_losses += env.losses
-        data.total_draws += env.draws
-
-        if episode % verbose_step == 0:
-            print("stats: ", data.total_wins, "/", data.total_draws, "/", data.total_losses)
+    evaluator = Evaluate(best_model_path, target_model_path, 100)
+    evaluator.load_models_and_set_env()
+    evaluator.battle()
 
 
 if __name__ == "__main__":
-    model_path = "./ckpt/ckpt-63100"
-    target_path = "./ckpt/ckpt-47000"
+    model_path = "./best_models/model_sap_gym_sb3_280822_finetuned_641057_steps"
+    target_path = "./best_models/model_sap_gym_sb3_200822_422718_steps"
     apply(model_path, target_path, test_episodes=100)
