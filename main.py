8fa877dd2707ab986cc6aebdcf89461c52b35113
André Pedersen <andrped94@gmail.com>
2023-03-31 16:44:51 +0200

    improved argparse [no ci]

--- main.py
+++ main.py
@@ -7,32 +7,32 @@ import logging as log
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--task', metavar='--t', type=str, nargs='?', default="train",
+    parser.add_argument('-t', '--task', type=str, nargs='?', default="train",
                         help="which task to perform - either 'train' or 'eval'.")
-    parser.add_argument('--nb_steps', metavar='--nt', type=int, nargs='?', default=10000,
+    parser.add_argument('-nt', '--nb_steps', type=int, nargs='?', default=10000,
                         help="number of time steps when training RL model.")
-    parser.add_argument('--nb_games', metavar='--ng', type=int, nargs='?', default=10000,
+    parser.add_argument('-ng', '--nb_games', type=int, nargs='?', default=10000,
                         help="number of games to run when evaluating model after training.")
-    parser.add_argument('--finetune', metavar='--fn', type=str, nargs='?', default=None,
+    parser.add_argument('-fn', '--finetune', type=str, nargs='?', default=None,
                         help="whether to finetune using pretrained model. Provide path to model if yes.")
-    parser.add_argument('--model_name', metavar='--m', type=str, nargs='?', default="rl_model",
+    parser.add_argument('-m', '--model_name', type=str, nargs='?', default="rl_model",
                         help="which name to use for the model.")
-    parser.add_argument('--nb_retries', metavar='--nr', type=int, nargs='?', default=1,
+    parser.add_argument('-nr', '--nb_retries', type=int, nargs='?', default=1,
                         help="number of times training is restarted (continued) if it crashes. Set to -1 to train "
                              "forever.")
-    parser.add_argument('--infer_model', metavar='-im', type=str, nargs='?', default=None,
+    parser.add_argument('-im', '--infer_model', type=str, nargs='?', default=None,
                         help="which model to use for deployment (give full path, without extension '.zip'.")
-    parser.add_argument('--infer_pversion', metavar='-py', type=str, nargs='?', default="3.7",
+    parser.add_argument('-py', '--infer_pversion', type=str, nargs='?', default="3.7",
                         help="define which python version the current deployment model is trained with.")
-    parser.add_argument('--batch_size', metavar='--bs', type=int, nargs='?', default=512,
+    parser.add_argument('-bs', '--batch_size', type=int, nargs='?', default=512,
                         help="set which batch size to use for training.")
-    parser.add_argument('--learning_rate', metavar='--lr', type=float, nargs='?', default=0.0003,
+    parser.add_argument('-lr', '--learning_rate', type=float, nargs='?', default=0.0003,
                         help="set which learning rate to use for training.")
-    parser.add_argument('--save_freq', metavar='--sf', type=int, nargs='?', default=10000,
+    parser.add_argument('-sf', '--save_freq', type=int, nargs='?', default=10000,
                         help="set frequency of how often models are saved using checkpoint callback.")
-    parser.add_argument('--gamma', metavar='--gm', type=float, nargs='?', default=0.99,
+    parser.add_argument('-gm', '--gamma', type=float, nargs='?', default=0.99,
                         help="set which gamma to use for MaskablePPO training.")
-    parser.add_argument('--verbose', metavar='--v', type=int, nargs='?', default=1,
+    parser.add_argument('-v', '--verbose', type=int, nargs='?', default=1,
                         help="sets the verbose level.")
     ret = parser.parse_known_args(sys.argv[1:])[0]
 
