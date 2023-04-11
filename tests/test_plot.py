bcf5a35c88d51617c15ca36f4f72e0a8e25058b2
andreped <andrped94@gmail.com>
2023-04-11 19:28:04 +0200

    Add pytest fixture to run plot test after train test

--- tests/test_plot.py
+++
@@ -1,5 +0,0 @@
-from smp.plot_history import plot_logs
-
-
-def test_train():
-    plot_logs()
--- tests/test_train.py
+++ tests/test_train.py
@@ -1,5 +1,12 @@
+from smp.plot_history import plot_logs
 from main import main
 
 
+@pytest.fixture
+def test_plot():
+    plot_logs()
+
+
+@pytest.mark.usefixtures("test_plot")
 def test_train():
     main()
