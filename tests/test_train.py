c6fb708ae219c6e1588bccebcd3772e9f5de6371
andreped <andrped94@gmail.com>
2023-04-11 19:40:38 +0200

    Attempt to change order to functional tests to get plot last

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,13 +1,10 @@
 from smp.plot_history import plot_logs
 from main import main
-import pytest
 
 
-@pytest.fixture
-def test_plot():
-    plot_logs()
-
-
-@pytest.mark.usefixtures("test_plot")
 def test_train():
     main()
+
+
+def test_plot():
+    plot_logs()
