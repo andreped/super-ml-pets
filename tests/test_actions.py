2cb5c3701b94273f360d3752c6452457759e5be6
andreped <andrped94@gmail.com>
2023-04-16 17:12:50 +0200

    Added tests for most actions

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -10,3 +10,17 @@ def test_class():
     sap.get_action_dict()
     sap.reorder(order=[list(range(5))])
     #sap.combine_in_team(n=[1, 1])
+    for i in range(5):
+        sap.buy(nth_slot=[i])
+
+    for j in [5, 6]:
+        sap.buy_food(nth_slot=[j], num_pets=5)
+    
+    sap.sell(nth_team_slot=[4])
+    sap.combine_in_team(n=[1, 2])
+    sap.buy_combine(n=[1, 2])
+    sap.freeze_unfreeze(nth_slot=2)
+
+    sap.roll()
+    sap.end_turn(0)
+
