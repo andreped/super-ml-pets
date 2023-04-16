225245d95313359fd297329be26c010774976378
andreped <andrped94@gmail.com>
2023-04-16 19:41:05 +0200

    Ignore __init__ Exception in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,8 +11,9 @@ try:
     disp = Display(visible=True, size=(1920, 1080), backend="xvfb", use_xauth=True)
     disp.start()
     pyautogui._pyautogui_x11._display = Xlib.display.Display(os.environ['DISPLAY'])
-except FileNotFoundError:  # Need to handle both regular and virtual displays for local and testing in the cloud
-    pass
+# Need to handle both regular and virtual displays for local and testing in the cloud 
+except FileNotFoundError:  # pragma: no cover
+    pass  # pragma: no cover
 
 # need to define logger here, as some tests require logger to be defined,
 # but it is only defined in main.py
