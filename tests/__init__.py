bb9c7a89ef72d632329e0f1ee348b1cbda372690
andreped <andrped94@gmail.com>
2023-04-16 15:34:51 +0200

    Try-catch for virtual display and local support

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,15 @@
 import pyautogui
 import os
-from pyvirtualdisplay.display import Display
-import Xlib
 
-# @TODO: This only works on Linux... Cannot run tests with virtual display
-disp = Display(visible=True, size=(1920, 1080), backend="xvfb", use_xauth=True)
-disp.start()
-pyautogui._pyautogui_x11._display = Xlib.display.Display(os.environ['DISPLAY'])
+try:
+    from pyvirtualdisplay.display import Display
+    import Xlib
+
+    # @TODO: This only works on Linux... Cannot run tests with virtual display
+    disp = Display(visible=True, size=(1920, 1080), backend="xvfb", use_xauth=True)
+    disp.start()
+    pyautogui._pyautogui_x11._display = Xlib.display.Display(os.environ['DISPLAY'])
+except FileNotFoundError:  # Need to handle both regular and virtual displays for local and testing in the cloud
+    pass
+
+# @TODO: Need to be able to gracefully kill the virtual display when finished
