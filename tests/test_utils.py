cac532a30429139530bda236794544f7123fb263
andreped <andrped94@gmail.com>
2023-04-16 14:49:32 +0200

    Tried setting up virtual display in CI

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,11 @@
 import pyautogui
 import os
-#from pyvirtualdisplay.display import Display
-#import Xlib.display
+from pyvirtualdisplay.display import Display
 
 # @TODO: This only works on Linux... Cannot run tests with virtual display
-#disp = Display(visible=True, size=(1920,1080), backend="xvfb", use_xauth=True)
-#disp.start()
-#pyautogui._pyautogui_x11._display = Xlib.display.Display(os.environ['DISPLAY'])
+disp = Display(visible=True, size=(1920, 1080), backend="xvfb", use_xauth=True)
+disp.start()
+pyautogui._pyautogui_x11._display = Xlib.display.Display(os.environ['DISPLAY'])
 
 from smp.utils import define_logger, opponent_generator, custom_easeOutQuad, \
     move_drag_tween, get_position, get_curr_screen_geometry, get_screen_scale
